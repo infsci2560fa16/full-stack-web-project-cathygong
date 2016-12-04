21611ed839b2541a1f1c35f7be090124ffb5cbfb
cathygong <cathygong@gmail.com>
2016-12-03 21:11:15 -0500

    ftl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,24 +38,14 @@ public class Main {
         Statement stmt = connection.createStatement();
         stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
         stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        //ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-        ResultSet rs = stmt.executeQuery("select * from userinfo, booklist, book_owner where booklist.bid=book_owner.bid and userinfo.uid=book_owner.uid and book_owner.status = 'TRUE' order by book_owner.post_date desc limit 3");
-
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
         ArrayList<String> output = new ArrayList<String>();
-        ArrayList<String> output2 = new ArrayList<String>();
         while (rs.next()) {
-          //output.add( "Read from DB: " + rs.getTimestamp("tick"));
-          output.add( "Read from DB: " + rs.getString("bname"));
-        }
-        
-        rs = stmt.executeQuery("select * from userinfo, booklist, book_owner where booklist.bid=book_owner.bid and userinfo.uid=book_owner.uid and book_owner.status = 'FALSE' order by book_owner.post_date desc limit 3");
-        while (rs.next()) {
-          output2.add( "Read from DB: " + rs.getString("author"));
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
         }
 
         attributes.put("results", output);
-        attributes.put("results2", output2);
         return new ModelAndView(attributes, "db.ftl");
       } catch (Exception e) {
         attributes.put("message", "There was an error: " + e);
@@ -72,8 +62,6 @@ public class Main {
       try{
       connection = DatabaseUrl.extract().getConnection();
       Statement stmt = connection.createStatement();
-      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS (user_email varchar(100),  user_password  varchar(30),  user_name  varchar(30) )");
-    //  stmt.executeUpdate("INSERT INTO users_info VALUES ('user_email','user_password','user_name')");
       ResultSet book_sale = stmt.executeQuery("select * from userinfo, booklist, book_owner where booklist.bid=book_owner.bid and userinfo.uid=book_owner.uid and book_owner.status='TRUE' order by book_owner.post_date desc limit 3");
       ArrayList<String> sale_bname = new ArrayList<String>();
       ArrayList<String> sale_author = new ArrayList<String>();
@@ -138,6 +126,103 @@ public class Main {
      attributes.put("need_pittid",need_pittid);
      attributes.put("need_postdate",need_postdate);
      return new ModelAndView(attributes, "home.ftl");
+     } catch (Exception e) {
+     attributes.put("message", "There was an error: " + e);
+     return new ModelAndView(attributes, "error.ftl");
+     } finally {
+     if (connection != null) try{connection.close();} catch(SQLException e){}
+    }}, new FreeMarkerEngine());
+    
+
+    get("/book_in_need", (req, res) ->
+    {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try{
+      connection = DatabaseUrl.extract().getConnection();
+      Statement stmt = connection.createStatement();
+      ResultSet book_need = stmt.executeQuery("select * from userinfo, booklist, book_owner where booklist.bid=book_owner.bid and userinfo.uid=book_owner.uid and book_owner.status = 'FALSE' order by book_owner.post_date desc"); 
+
+      ArrayList<String> need_bname = new ArrayList<String>();
+      ArrayList<String> need_author = new ArrayList<String>();
+      ArrayList<String> need_edition = new ArrayList<String>();
+      ArrayList<String> need_isbn13 = new ArrayList<String>();
+      ArrayList<String> need_isbn10 = new ArrayList<String>();
+      ArrayList<String> need_pittid = new ArrayList<String>();
+      ArrayList<String> need_postdate = new ArrayList<String>();
+      String text;
+
+    while(book_need.next())
+    {
+       need_bname.add(book_need.getString("bname"));
+       need_author.add(book_need.getString("author"));
+       text = book_need.getString("edition");
+       if(book_need.wasNull()) need_edition.add("Unspecified");
+       else need_edition.add(book_need.getString("edition"));
+       need_isbn13.add(book_need.getString("isbn13"));
+       need_isbn10.add(book_need.getString("isbn10"));
+       need_pittid.add(book_need.getString("pittid"));
+       text = book_need.getString("post_date");
+       if(book_need.wasNull()) need_edition.add("Unspecified");
+       else need_postdate.add(book_need.getString("post_date"));
+     }
+
+     attributes.put("need_bname",need_bname);
+     attributes.put("need_author",need_author);
+     attributes.put("need_edition",need_edition);
+     attributes.put("need_isbn13",need_isbn13);
+     attributes.put("need_isbn10",need_isbn10);
+     attributes.put("need_pittid",need_pittid);
+     attributes.put("need_postdate",need_postdate);
+     return new ModelAndView(attributes, "book_in_need.ftl");
+     } catch (Exception e) {
+     attributes.put("message", "There was an error: " + e);
+     return new ModelAndView(attributes, "error.ftl");
+     } finally {
+     if (connection != null) try{connection.close();} catch(SQLException e){}
+    }}, new FreeMarkerEngine());
+
+    get("/book_on_sale", (req, res) ->
+    {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try{
+      connection = DatabaseUrl.extract().getConnection();
+      Statement stmt = connection.createStatement();
+      ResultSet book_sale = stmt.executeQuery("select * from userinfo, booklist, book_owner where booklist.bid=book_owner.bid and userinfo.uid=book_owner.uid and book_owner.status='TRUE' order by book_owner.post_date desc limit 3");
+      ArrayList<String> sale_bname = new ArrayList<String>();
+      ArrayList<String> sale_author = new ArrayList<String>();
+      ArrayList<String> sale_edition = new ArrayList<String>();
+      ArrayList<String> sale_isbn13 = new ArrayList<String>();
+      ArrayList<String> sale_isbn10 = new ArrayList<String>();
+      ArrayList<String> sale_pittid = new ArrayList<String>();
+      ArrayList<String> sale_postdate = new ArrayList<String>();
+      String text;
+
+    while(book_sale.next())
+    {
+       sale_bname.add(book_sale.getString("bname"));
+       sale_author.add(book_sale.getString("author"));
+       text = book_sale.getString("edition");
+       if(book_sale.wasNull()) sale_edition.add("Unspecified");
+       else sale_edition.add(book_sale.getString("edition"));
+       sale_isbn13.add(book_sale.getString("isbn13"));
+       sale_isbn10.add(book_sale.getString("isbn10"));
+       sale_pittid.add(book_sale.getString("pittid"));
+       text = book_sale.getString("post_date");
+       if(book_sale.wasNull()) sale_edition.add("Unspecified");
+       else sale_postdate.add(book_sale.getString("post_date"));
+     }
+
+     attributes.put("sale_bname",sale_bname);
+     attributes.put("sale_author",sale_author);
+     attributes.put("sale_edition",sale_edition);
+     attributes.put("sale_isbn13",sale_isbn13);
+     attributes.put("sale_isbn10",sale_isbn10);
+     attributes.put("sale_pittid",sale_pittid);
+     attributes.put("sale_postdate",sale_postdate);
+
+     return new ModelAndView(attributes, "book_on_sale.ftl");
      } catch (Exception e) {
      attributes.put("message", "There was an error: " + e);
      return new ModelAndView(attributes, "error.ftl");
